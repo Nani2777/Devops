73a3fed58d9b245b97ddf6b562c3ec940b17a4a6
harish <harish.chilamanthula@gmail.com>
2019-06-26 12:48:41 +0530

    setup

--- router.js
+++ router.js
@@ -1,13 +1,13 @@
 let express = require('express');
 let router = express.Router();
 
-const drishti = require('./drishti');
-const encryptionApi = require('./encryption');
+//const drishti = require('./drishti');
+//const encryptionApi = require('./encryption');
 const webhookscript = require('./webhook');
 const feedback = require('./IFL_Feedback');
 
-router.use('/drishti', drishti);
-router.use('/encrypt', encryptionApi);
+//router.use('/drishti', drishti);
+//router.use('/encrypt', encryptionApi);
 router.use('/webhook', webhookscript);
 router.use('/feedback', feedback);
 module.exports = router;
