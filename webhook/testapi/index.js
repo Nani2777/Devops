822114e8285f3b23febbaf77ead4f79dd2ea489d
harish <harish.chilamanthula@gmail.com>
2019-07-16 14:54:14 +0530

    added test code

--- webhook/testapi/index.js
+++ webhook/testapi/index.js
@@ -22,4 +22,21 @@ router.post('/implwebhook', function (req, res) {
     res.end("OK");
   });
 
+  router.get('/implwebhook', function (req, res) {
+    console.log('chat logs');
+    console.log(req.body);
+    //console.log(req.query);
+    //console.log(req.params);
+    //console.log(req.headers);
+    //Logger.info(req.body);
+    //Logger.info(req.query);
+    //Logger.info(req.params);
+    //Logger.info(req.headers);
+    winston.error('error');
+    winston.info('working'); 
+    console.log(req.body);
+    res.writeHead(200);
+    res.end("OK");
+  });
+
 module.exports = router;
