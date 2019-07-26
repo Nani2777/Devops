61dd0d2f32afed0eea73593d7075d0c8124e246b
harish <harish.chilamanthula@gmail.com>
2019-07-26 16:28:58 +0530

    adding code to test

--- webhook/testapi/index.js
+++ webhook/testapi/index.js
@@ -40,9 +40,26 @@ router.post('/implwebhook', function (req, res) {
       var webhookData = req.query;
       console.log('Oracle Email -', webhookData);
       if(typeof(webhookData) == 'object'){
-        webhookData.forEach(function(each){
-          console.log(each);
-        })
+        var complete_data = webhookData.cmpdetails.split(',');
+        let custom_params = complete_data.reduce((object, key) => {
+          var z = key.split(':')
+          object[z[0]] = z[1];
+          return object;
+        }, {});
+        console.log(custom_params)
+        //var comp_id = custom_params['cmpid']
+        var comp_id = '107a3b41-1aa3-45c6-a324-f0399a2aa2af'
+        var vid = custom_params['vid']
+        console.log('Final----',comp_id,custom_params)
+        /*var url = "http://evbk.gamooga.com/ev/?c=" + comp_id + "&v=" + vid + "&e=" + event
+        Object.entries(complete_data).forEach(
+          ([key,value]) => {
+            if(key != 'cmpid' && key != 'vid'){
+              console.log(key,value)
+              url = url + '&ky=' + key + '&vl=' + value + '&tp=s'
+            }
+          }
+        )*/
       }
       res.writeHead(200);
       res.end("OK");
