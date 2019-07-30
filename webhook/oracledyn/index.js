b19b390a441ac157beb9f0b9055a16b16aea66ee
harish <harish.chilamanthula@gmail.com>
2019-07-30 17:39:40 +0530

    changes

--- webhook/oracledyn/index.js
+++ webhook/oracledyn/index.js
@@ -4,34 +4,34 @@ const axios = require('axios');
 
 router.get('/espcallback/', function (req, res) {
     console.log('chat post logs');
-    console.log(req.body);
-    console.log(req.query);
-    console.log(req.params);
-    console.log(req.headers);
+    //console.log(req.body);
+    //console.log(req.query);
+    //console.log(req.params);
+    //console.log(req.headers);
     //winston.error('error');
     //winston.info('working'); 
-    console.log(req.body);
+    //console.log(req.body);
     try {
-      var webhookData = req.query;
+      let webhookData = req.query;
       console.log('Oracle Email -', webhookData);
       if(typeof(webhookData) == 'object'){
-        var complete_data = webhookData.cmpdetails.split(',');
+        let complete_data = webhookData.cmpdetails.split(',');
         let custom_params = complete_data.reduce((object, key) => {
-          var z = key.split(':')
+          let z = key.split(':')
           object[z[0]] = z[1];
           return object;
         }, {});
-        console.log(custom_params)
-        var comp_id = custom_params['cmpid']
-        //var comp_id = '107a3b41-1aa3-45c6-a324-f0399a2aa2af'
-        var vid = custom_params['vid']
-        console.log('Final----',comp_id,custom_params)
-        var event = 'test'
-        var url = `http://evbk.gamooga.com/ev/?c=${comp_id}&v=${vid}&e=${event}&ky=email&vl=${webhookData.email}&tp=s&ky=bouncerule&vl=${webhookData.bouncerule}&tp=s&ky=bouncetype&vl=${webhookData.bouncetype}&tp=s`
+        //console.log(custom_params)
+        let comp_id = custom_params['cmpid']
+        //let comp_id = '107a3b41-1aa3-45c6-a324-f0399a2aa2af'
+        let vid = custom_params['vid']
+        //console.log('Final----',comp_id,custom_params)
+        let event = 'test'
+        let url = `http://evbk.gamooga.com/ev/?c=${comp_id}&v=${vid}&e=${event}&ky=email&vl=${webhookData.email}&tp=s&ky=bouncerule&vl=${webhookData.bouncerule}&tp=s&ky=bouncetype&vl=${webhookData.bouncetype}&tp=s`
         Object.entries(custom_params).forEach(
           ([key,value]) => {
             if(key != 'cmpid' && key != 'vid'){
-              console.log(key,value)
+              //console.log(key,value)
               url = url + '&ky=' + key + '&vl=' + value + '&tp=s'
             }
           }
