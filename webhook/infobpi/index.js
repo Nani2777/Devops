1e9e6c930ba3e8e570dc0c84560cfdbe12d9dcd4
harish <harish.chilamanthula@gmail.com>
2019-08-22 12:31:34 +0530

    candere delivery script added

--- webhook/infobpi/index.js
+++ webhook/infobpi/index.js
@@ -14,8 +14,6 @@ router.post('/warplycallback/', function (req, res) {
   //Logger.info(req.query);
   //Logger.info(req.params);
   //Logger.info(req.headers);
-  winston.error('error');
-  winston.info('working'); 
   //console.log(req.body);
   res.writeHead(200);
   res.end("OK");
@@ -31,12 +29,39 @@ router.get('/warplycallback/', function (req, res) {
     //Logger.info(req.query);
     //Logger.info(req.params);
     //Logger.info(req.headers);
-    winston.error('error');
-    winston.info('working'); 
     //console.log(req.body);
     res.writeHead(200);
     res.end("OK");
   });
 
+router.post('/wasmscallback/', function (req, res) {
+  console.log('post logs');
+  console.log(req.body);
+  //console.log(req.query);
+  //console.log(req.params);
+  //console.log(req.headers);
+  //Logger.info(req.body);
+  //Logger.info(req.query);
+  //Logger.info(req.params);
+  //Logger.info(req.headers);
+  //console.log(req.body);
+  res.writeHead(200);
+  res.end("OK");
+});
+
+router.get('/wasmscallback/', function (req, res) {
+    console.log('post logs');
+    console.log(req.body);
+    //console.log(req.query);
+    //console.log(req.params);
+    //console.log(req.headers);
+    //Logger.info(req.body);
+    //Logger.info(req.query);
+    //Logger.info(req.params);
+    //Logger.info(req.headers);
+    //console.log(req.body);
+    res.writeHead(200);
+    res.end("OK");
+  });
 
 module.exports = router;
