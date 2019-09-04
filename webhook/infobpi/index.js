92bb71c2a95c5a29e01da38e8d9c2cf0c8ab152b
Govardhan <govardhans@gamooga.com>
2019-09-04 19:20:07 +0530

    changed in infobpi

--- webhook/infobpi/index.js
+++ webhook/infobpi/index.js
@@ -5,6 +5,7 @@ const eventpush = require('../eventpush');
 
 
 router.post('/warplycallback/', function (req, res) {
+  console.log('/warplycallback/');
   console.log('post logs');
   console.log(req.body);
   //console.log(req.query);
@@ -20,6 +21,7 @@ router.post('/warplycallback/', function (req, res) {
 });
 
 router.get('/warplycallback/', function (req, res) {
+  console.log('/warplycallback/');
     console.log('get logs');
     console.log(req.body);
     //console.log(req.query);
@@ -35,52 +37,9 @@ router.get('/warplycallback/', function (req, res) {
   });
 
 router.post('/wasmscallback/', function (req, res) {
+  console.log('/wasmscallback/');
   console.log('post logs');
   console.log(req.body);
-  console.log('Pepipost Email -', req.body);
-  try {
-    var webhookData = req.body;
-    console.log(typeof webhookData);
-    if (typeof (webhookData) == 'object') {
-        webhookData.forEach(function (each) {
-                try {
-                    var cmp_data = each['X-APIHEADER'];
-                    var evname = each.EVENT.toLowerCase();
-                    var eventname;
-                    if(evname == 'sent'){
-                     eventname='delivered';
-                    }else{
-                     eventname = evname;
-                    }
-                    let exe = cmp_data.split(':');
-                    let details = {};
-                       details['compid']= exe[0];
-                       details['vid']= exe[1];
-                       details['cp_type']= exe[2];
-                       details['cp_id']= exe[3];
-                       details['tp']= exe[4];
-                       details['tpid']= exe[5];
-                       details['email'] = each.EMAIL,
-                       details['event'] = eventname,
-                       details['server'] = 'evbk.gamooga.com'
-                       eventpush(details);
-                       console.log(details, '------Sent to eventpush');
-                } catch (err) {
-                    console.log(comp_id,'Pepipost - Error in entries for the Pepipost req data', req.body);
-                    //Log.L(Log.E, comp_id,'Pepipost - Error in entries for the Pepipost req data', req.body);
-                    res.writeHead(200);
-                    res.end("ERROR");
-                }
-        })
-    }
-    res.writeHead(200);
-    res.end("OK");
-} catch (err) {
-    console.log('Pepipost - Error in Webhook from Pepipost \n%s', err);
-    //Log.L(Log.E, 'Pepipost - Error in Webhook from Pepipost \n%s', err);
-    res.writeHead(200);
-    res.end("ERROR");
-}
   //console.log(req.query);
   //console.log(req.params);
   //console.log(req.headers);
@@ -94,6 +53,7 @@ router.post('/wasmscallback/', function (req, res) {
 });
 
 router.get('/wasmscallback/', function (req, res) {
+  console.log('/wasmscallback/');
     console.log('get logs');
     console.log(req.body);
     //console.log(req.query);
@@ -107,54 +67,4 @@ router.get('/wasmscallback/', function (req, res) {
     res.writeHead(200);
     res.end("OK");
   });
-
-
-  router.post('/esppostback/', async (req, res) => {
-    try {
-        var webhookData = req.body;
-        console.log(typeof webhookData);
-        if (typeof (webhookData) == 'object') {
-            webhookData.forEach(function (each) {
-                    try {
-                        var cmp_data = each['X-APIHEADER'];
-                        var evname = each.EVENT.toLowerCase();
-                        var eventname;
-                        if(evname == 'sent'){
-                         eventname='delivered';
-                        }else{
-                         eventname = evname;
-                        }
-                        let exe = cmp_data.split(':');
-                        let details = {};
-                           details['compid']= exe[0];
-                           details['vid']= exe[1];
-                           details['cp_type']= exe[2];
-                           details['cp_id']= exe[3];
-                           details['tp']= exe[4];
-                           details['tpid']= exe[5];
-                           details['email'] = each.EMAIL,
-                           details['event'] = eventname,
-                           details['server'] = 'evbk.gamooga.com'
-                           eventpush(details);
-                           console.log(details, '------Sent to eventpush');
-                    } catch (err) {
-                        console.log(comp_id,'Pepipost - Error in entries for the Pepipost req data', req.body);
-                        //Log.L(Log.E, comp_id,'Pepipost - Error in entries for the Pepipost req data', req.body);
-                        res.writeHead(200);
-                        res.end("ERROR");
-                    }
-            })
-        }
-        res.writeHead(200);
-        res.end("OK");
-    } catch (err) {
-        console.log('Pepipost - Error in Webhook from Pepipost \n%s', err);
-        //Log.L(Log.E, 'Pepipost - Error in Webhook from Pepipost \n%s', err);
-        res.writeHead(200);
-        res.end("ERROR");
-    }
-    //res.writeHead(200);
-      //  res.end("OK");
-});
-
 module.exports = router;
