7063eea0972aa5eebb957ed97d3845ae23ca207d
Govardhan <govardhans@gamooga.com>
2019-10-25 12:55:50 +0530

    mailgun add

--- webhook/testapi/index.js
+++ webhook/testapi/index.js
@@ -23,6 +23,42 @@ const eventpush = require('../eventpush')
   res.end("OK");
 });*/
 
+
+//Planetsuper heros Mailgun
+router.get('/mailgun', function (req, res) {
+  console.log('mailgun get logs');
+  console.log(req.body);
+  console.log(req.query);
+  console.log(req.params);
+  console.log(req.headers);
+  //Logger.info(req.body);
+  //Logger.info(req.query);
+  //Logger.info(req.params);
+  //Logger.info(req.headers);
+  //winston.error('error');
+  //winston.info('working'); 
+  //console.log(req.body);
+  res.writeHead(200);
+  res.end("OK");
+});
+
+router.post('/mailgun', function (req, res) {
+  console.log('mailgun post logs');
+  console.log(req.body);
+  console.log(req.query);
+  console.log(req.params);
+  console.log(req.headers);
+  //Logger.info(req.body);
+  //Logger.info(req.query);
+  //Logger.info(req.params);
+  //Logger.info(req.headers);
+  //winston.error('error');
+  //winston.info('working'); 
+  //console.log(req.body);
+  res.writeHead(200);
+  res.end("OK");
+});
+
 router.get('/chatwebhook', function (req, res) {
   console.log('chat post logs');
   console.log(req.body);
