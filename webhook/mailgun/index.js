d6021313109e69f3e2f74565bbc5efaa840b8098
nani0115 <harish.chilamanthula@gmail.com>
2020-01-09 15:39:46 +0530

    mailgun logs

--- webhook/mailgun/index.js
+++ webhook/mailgun/index.js
@@ -6,7 +6,7 @@ const log = require('../../logger');
 router.post('/espcallback/', function (req, res) {
     console.log('Mailgun post logs');
     console.log(req.query);
-    log.info(req.query);
+    log.info(req);
     console.log('Body <><><><><><>',req.body);
     res.writeHead(200);
     res.end("OK");
