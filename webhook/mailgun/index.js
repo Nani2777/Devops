632d2bd304eb8c1ed6926bc9ebe5006ba998a1c7
nani0115 <harish.chilamanthula@gmail.com>
2020-01-17 11:56:13 +0530

    changes in mailgun

--- webhook/mailgun/index.js
+++ webhook/mailgun/index.js
@@ -8,6 +8,7 @@ router.post('/espcallback/', function (req, res) {
     console.log(req);
     //log.info(req);
     log.info(JSON.stringify(req.body));
+    log.info(JSON.stringify(req));
     console.log('Body <><><><><><>',req.body);
     res.writeHead(200);
     res.end("OK");
