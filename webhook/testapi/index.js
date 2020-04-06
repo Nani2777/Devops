ba8e461bd53df4d593d38696adbd663ee3119329
nani0115 <harish.chilamanthula@gmail.com>
2020-04-06 17:01:34 +0530

    karix event

--- webhook/testapi/index.js
+++ webhook/testapi/index.js
@@ -276,6 +276,48 @@ router.get('/implwebhook/unsubscribe/', function (req, res) {
   }
 });
 
+
+router.post('/wpsmscallback/', async (req, res) => {
+  let wtappsms = req.body;
+log.info(typeof wtappsms);
+console.log('body<><><><><><><><><><><><>',req.body);
+try{
+    if (typeof (wtappsms) == 'object') {
+        log.info(wtappsms);
+        let extra = wtappsms.recipient.reference.messageTag1;
+        let event_type = wtappsms.events.eventType;
+        if (event_type  == 'DELIVERY EVENTS'){//status == "Read" || status == "delivered" || status == "sent"){
+            console.log(extra)
+            var params = extra.split(';').reduce((arr, each) => {
+              arr[each.split(':')[0]] = each.split(':')[1]
+              return arr
+            },{})
+            console.log(params)
+            if(params['compid'] == '6a7ba941-3460-4ff6-b36b-1e1d214415c5'){
+              params['server'] = 'engageb.rsec.co.in';
+            } else if(params['compid']=='fcbe3928-6512-48a6-8cb5-c8c51e100539'){
+              params['server'] = 'js3in1.gamooga.com';
+            }else{
+              params['server'] = 'evbk.gamooga.com';
+            }
+            var details = Object.assign({},params,{event_type,status:wtappsms.notificationAttributes.status,reason:wtappsms.notificationAttributes.reason,channel:wtappsms.channel,to_add:wtappsms.recipient.to,event:'_wpsms_'+wtappsms.notificationAttributes.status})
+              
+              //{...params,event_type,status:wtappsms.notificationAttributes.status,reason:wtappsms.notificationAttributes.reason,channel:wtappsms.channel,to_add:wtappsms.recipient.to,event:'_wpsms_'+wtappsms.notificationAttributes.status}
+            console.log(details);
+            log.info(details);
+            eventpush(details);
+        }
+    }
+  }catch (e){
+      log.info("Error in incoming data from value first", e);
+      res.writeHead(200);
+      res.end("ERROR");
+  }
+  res.writeHead(200);
+  res.end("OK");
+});
+
+/*
 router.post('/wpsmscallback', async (req, res) => {
     let wtappsms = req.body
     console.log(wtappsms)
@@ -319,7 +361,7 @@ router.post('/wpsmscallback', async (req, res) => {
     }
 })
  
-/*
+
   try {
       if (typeof (wtappsms) == 'object') {
           log.info(wtappsms);
