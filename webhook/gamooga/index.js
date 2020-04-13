7e0a63c8f960c7ea7324e16f2dcd15b1d1f31680
nani0115 <harish.chilamanthula@gmail.com>
2020-04-13 19:18:10 +0530

    capmaign details for karix wrapper

--- webhook/gamooga/index.js
+++ webhook/gamooga/index.js
@@ -1,11 +1,4 @@
-//import DateTimeUtil from '../util/datetime'
 const Url = require('url');
-//import nunjucks from 'nunjucks'
-//import NunjucksEnv from './nunjucks_custom'
-//import VisProp from '../vprop'
-//import NodeUtil from 'util'
-//import Log from '../util/log'
-//import LRU from 'lru-cache'
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
@@ -21,12 +14,17 @@ router.post('/wpsmscallback/', function (req, res) {
     let eventToTrigger = '_wpsms_click';
     let compid = body.compid;
     let vid = body.vid;
+    let cp_id = body.cp_id;
+    let cp_type = body.cp_type;
+    let tp = body.tp;
+    let tpid = body.tpid;
+    let runid = body.runid;
     let td = new Date().getTime() + '' + parseInt(Math.random()*10000);
     let url = body.url;//'https://www.lidolearning.com/?utmcampaign=testcamp';    
     let urlToWrap;
     let urlObj = Url.parse(url, true, true);
     urlToWrap = urlObj.format(urlObj);
-    let wrappedurl = 'http://'+'evbk.gamooga.com'+'/ev/?e='+eventToTrigger+'&c='+compid+'&v='+vid+'&s=abc&t=xyz&z='+td+'&redir='+urlToWrap;
+    let wrappedurl = 'http://'+'evbk.gamooga.com'+'/ev/?e='+eventToTrigger+'&c='+compid+'&v='+vid+'&ky=cp_id&vl='+cp_id+'tp=s&ky=cp_type&vl='+cp_type+'tp=s&ky=tp&vl='+tp+'tp=s&ky=tpid&vl='+tpid+'tp=s&ky=runid&vl='+runid+'tp=s&s=abc&t=xyz&z='+td+'&redir='+urlToWrap;
     console.log(wrappedurl);
     res.status(200).json({ 'url' : encodeURIComponent(wrappedurl)});//send(wrappedurl);
 });
