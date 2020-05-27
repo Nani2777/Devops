9beec617e49b3849a8e9c69afa26f053c0c511b6
nani0115 <harish.chilamanthula@gmail.com>
2020-05-27 19:19:46 +0530

    candere API

---
+++ webhook/candere/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const eventpush = require('../eventpush');
+//const Log = require('stark/utils/log');
+const log = require('../../logger');
+
+
+router.post('/evcallback/', async (req, res) => {
+    let data = req.body;
+    log.info(typeof data);
+    console.log('post<><><><><><><><><><><><>',req.body);
+    res.writeHead(200);
+    res.end("OK");
+});
+
+router.get('/evcallback/', async (req, res) => {
+    let data = req.body;
+    log.info(typeof data);
+    console.log('get<><><><><><><><><><><><>',req.body);
+    res.writeHead(200);
+    res.end("OK");
+});
+
+
+module.exports = router;
