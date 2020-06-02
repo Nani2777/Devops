d3f24d0c686ac05770c156fdac6c32e7e3e24af2
nani0115 <harish.chilamanthula@gmail.com>
2020-06-02 13:44:53 +0530

    candere comments

--- webhook/candere/index.js
+++ webhook/candere/index.js
@@ -10,6 +10,10 @@ router.post('/evcallback/', async (req, res) => {
     let data = req.body;
     log.info(typeof data,' candere post<><><><><><><><><><><><> ', data);
     console.log('post<><><><><><><><><><><><>',req.body);
+    log.info('candere body',req.body);
+    log.info('candere query',req.query);
+    log.info('candere params',req.params);
+    log.info('candere headers',req.headers);
     res.writeHead(200);
     res.end("OK");
 });
@@ -17,6 +21,10 @@ router.post('/evcallback/', async (req, res) => {
 router.get('/evcallback/', async (req, res) => {
     let data = req.query;
     log.info(typeof data,' candere get<><><><><><><><><><><><> ', data);
+    log.info('candere body',req.body);
+    log.info('candere query',req.query);
+    log.info('candere params',req.params);
+    log.info('candere headers',req.headers);
     console.log('get<><><><><><><><><><><><>',req.query);
     res.writeHead(200);
     res.end("OK");
