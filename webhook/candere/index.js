414c2e108f0f98568d826809c94602d95605ac97
nani0115 <harish.chilamanthula@gmail.com>
2020-06-04 18:23:32 +0530

    changes for candere

--- webhook/candere/index.js
+++ webhook/candere/index.js
@@ -9,23 +9,39 @@ const log = require('../../canderelogs');
 router.post('/evcallback/', async (req, res) => {
     let data = req.body;
     log.info(typeof data,' candere post<><><><><><><><><><><><> ', data);
-    console.log('post<><><><><><><><><><><><>',req.body);
-    log.info('candere body',req.body);
-    log.info('candere query',req.query);
-    log.info('candere params',req.params);
-    log.info('candere headers',req.headers);
-    res.writeHead(200);
-    res.end("OK");
-});
-
-router.get('/evcallback/', async (req, res) => {
-    let data = req.query;
-    log.info(typeof data,' candere get<><><><><><><><><><><><> ', data);
-    log.info('candere body',req.body);
-    log.info('candere query',req.query);
-    log.info('candere params',req.params);
-    log.info('candere headers',req.headers);
-    console.log('get<><><><><><><><><><><><>',req.query);
+    console.log(typeof data,'body<><><><><><><><><><><><>',req.body);
+    const renameKey = (object, key, newKey) => {
+        const clonedObj = clone(object);                  
+        const targetKey = clonedObj[key];
+        delete clonedObj[key];
+        clonedObj[newKey] = targetKey;
+        return clonedObj;
+    };
+    const clone = (obj) => Object.assign({}, obj);
+    try{
+        if (typeof (data) == 'object') {
+            // data['compid'] = '107a3b41-1aa3-45c6-a324-f0399a2aa2af';
+            data['compid'] = 'f7cfe791-dcc0-4c2c-a233-a5ca65229915';
+            data['uid'] = data['Email_id']
+            data = renameKey(data, 'eventName', 'event');
+            if(data['compid'] == '6a7ba941-3460-4ff6-b36b-1e1d214415c5'){
+                data['server'] = 'engageb.rsec.co.in';
+            } else if(data['compid']=='fcbe3928-6512-48a6-8cb5-c8c51e100539'){
+                data['server'] = 'js3in1.gamooga.com';
+            }else{
+                data['server'] = 'evbk.gamooga.com';
+            }
+            console.log(data);
+            log.info(data);
+            setTimeout(function(){
+                eventpush(data);
+            },10000)
+        }
+    }catch (e){
+        log.info("Error in incoming data from value first", e);
+        res.writeHead(200);
+        res.end("ERROR");
+    }
     res.writeHead(200);
     res.end("OK");
 });
--- webhook/eventpush.js
+++ webhook/eventpush.js
@@ -3,11 +3,37 @@ const log = require('../logger');
 
 const eventpush = function(data){
   try{
-    let compid = data.comp_id ? data.comp_id : data.compid;
-    var url = `http://${data.server}/ev/?c=${compid}&v=${data.vid}&e=${data.event}`
+    if(data.vid){
+      let compid = data.comp_id ? data.comp_id : data.compid;
+      var url = `http://${data.server}/ev/?c=${compid}&v=${data.vid}&e=${data.event}`
         Object.entries(data).forEach(
           ([key,value]) => {
             if(key != 'compid' && key != 'comp_id' && key != 'vid' && key != 'event' && key != 'server'){
+              //log.info(key,value)
+              url = url + '&ky=' + key.toLowerCase() + '&vl=' + value + '&tp=s'
+            }
+          }
+        )
+        log.info(url);
+        console.log(url);
+        //return url
+        try{
+          axios.get(url).then(function(response) {
+            log.info(response.statusText);
+          })
+          .catch(function(error) {
+            log.info(error);
+          });
+        }catch(e){
+          log.info("Error in event push", e);
+        }
+    }
+    else if(data.uid){
+      let compid = data.comp_id ? data.comp_id : data.compid;
+      var url = `http://${data.server}/evwid/?c=${compid}&u=${data.uid}&e=${data.event}`
+        Object.entries(data).forEach(
+          ([key,value]) => {
+            if(key != 'compid' && key != 'comp_id' && key != 'uid' && key != 'event' && key != 'server'){
               //log.info(key,value)
               url = url + '&ky=' + key + '&vl=' + value + '&tp=s'
             }
@@ -26,11 +52,12 @@ const eventpush = function(data){
         }catch(e){
           log.info("Error in event push", e);
         }
-      } catch (e){
-        log.info("Error in incoming data from value first", e);
-        //res.writeHead(200);
-        //res.end("ERROR");
-      }
+    }
+  } catch (e){
+    log.info("Error in incoming data from value first", e);
+    //res.writeHead(200);
+    //res.end("ERROR");
+  }
 }
 
 module.exports = eventpush;
