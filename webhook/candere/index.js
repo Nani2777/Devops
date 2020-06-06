defb15e1fab3773c6517501080edf32840e4b904
nani0115 <harish.chilamanthula@gmail.com>
2020-06-07 01:07:48 +0530

    candere code

--- webhook/candere/index.js
+++ webhook/candere/index.js
@@ -8,8 +8,7 @@ const log = require('../../canderelogs');
 
 router.post('/evcallback/', async (req, res) => {
     let data = req.body;
-    log.info(typeof data,' candere post<><><><><><><><><><><><> ', data);
-    console.log(typeof data,'body<><><><><><><><><><><><>',req.body);
+    log.info(typeof data,' candere ', data);
     const renameKey = (object, key, newKey) => {
         const clonedObj = clone(object);                  
         const targetKey = clonedObj[key];
@@ -18,27 +17,40 @@ router.post('/evcallback/', async (req, res) => {
         return clonedObj;
     };
     const clone = (obj) => Object.assign({}, obj);
+    async function sleep(ms) {
+        return new Promise((resolve, reject) => {
+            setTimeout(resolve,ms)
+            index = -1;
+        })
+    }
     try{
         if (typeof (data) == 'object') {
-            // data['compid'] = '107a3b41-1aa3-45c6-a324-f0399a2aa2af';
-            data['compid'] = 'f7cfe791-dcc0-4c2c-a233-a5ca65229915';
-            data['uid'] = data['Email_id']
-            data = renameKey(data, 'eventName', 'event');
-            if(data['compid'] == '6a7ba941-3460-4ff6-b36b-1e1d214415c5'){
-                data['server'] = 'engageb.rsec.co.in';
-            } else if(data['compid']=='fcbe3928-6512-48a6-8cb5-c8c51e100539'){
-                data['server'] = 'js3in1.gamooga.com';
-            }else{
-                data['server'] = 'evbk.gamooga.com';
-            }
-            console.log(data);
-            log.info(data);
-            setTimeout(function(){
-                eventpush(data);
-            },10000)
+            console.log(Object.keys(data).length)
+            var values = Object.keys(data).map(function (key) { return data[key]; });
+            let date_ob = new Date();
+            let index = -1;
+            values.forEach(async (each) => {
+                index++
+                await sleep(index*10000)
+                // console.log(each,'<><><><><><>',new Date())
+                each['uid'] = each['Email_id']
+                each = renameKey(each, 'eventName', 'event');
+                each['compid'] = '107a3b41-1aa3-45c6-a324-f0399a2aa2af';
+                if(each['compid'] == '6a7ba941-3460-4ff6-b36b-1e1d214415c5'){
+                    each['server'] = 'engageb.rsec.co.in';
+                } else if(each['compid']=='fcbe3928-6512-48a6-8cb5-c8c51e100539'){
+                    each['server'] = 'js3in1.gamooga.com';
+                }else{
+                    each['server'] = 'evbk.gamooga.com';
+                }
+                log.info(each,'<><><><><><>',new Date())
+                console.log(each,'<><><><><><>',new Date())
+                eventpush(each)
+            })
         }
     }catch (e){
         log.info("Error in incoming data from value first", e);
+        console.log("Error in incoming data from value first", e)
         res.writeHead(200);
         res.end("ERROR");
     }
